35871b1b31e3215a7810acf5f7b9c299153551a1
Octavia_Eka <istudyaccz@gmail.com>
2025-05-03 09:42:47 +0000

    onboard (not finished yet) + lil bit auth page

---
+++ lib/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:wanderin/onboarding.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Onboarding()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/gradient.png'), fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/logo.png', width: 139, height: 139),
+                const SizedBox(height: 24),
+                const Text(
+                  'W A N D E R I N',
+                  style: TextStyle(
+                    // fontFamily: ,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 3,
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
